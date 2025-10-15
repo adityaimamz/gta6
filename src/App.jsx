5f2b00b5a312e33d38497f197414c224c2159acb
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2025-10-15 16:50:55 +0700

    Add new sections for Coming Soon, Final, First Video, Hero, Jason, Lucia, NavBar, Outro, PostCard, and Second Video with GSAP animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import gsap from "gsap"
-import { ScrollTrigger } from "gsap/all"
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
-gsap.registerPlugin(ScrollTrigger)
+import NavBar from './sections/NavBar';
+import Hero from './sections/Hero';
+import FirstVideo from './sections/FirstVideo';
+import Jason from './sections/Jason';
+import SecondVideo from './sections/SecondVideo';
+import Lucia from './sections/Lucia';
+import PostCard from './sections/PostCard';
+import Final from './sections/Final';
+import Outro from './sections/Outro';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
-      <h1 className="text-6xl font-bold">Hello World</h1>
+      <NavBar />
+      <Hero />
+
+      <FirstVideo />
+      <Jason />
+
+      <SecondVideo />
+      <Lucia />
+
+      <PostCard />
+      <Final />
+      <Outro />
     </main>
   )
 }
